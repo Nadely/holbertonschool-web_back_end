84a5f12e75567ce7c423eb7e05532b0224466534
Nadely <6890@holbertonstudents.com>
2024-01-02 20:45:58 +0100

    Async Generator

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" write a coroutine called async_comprehension that takes no arguments.
+The coroutine will collect 10 random numbers using an async comprehensing
+over async_generator, then return the 10 random numbers."""
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+import asyncio
+
+
+async def async_comprehension():
+    """ return the 10 random numbers"""
---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""write a measure_runtime coroutine that will execute async_comprehension
+four times in parallel using asyncio.gather."""
+
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+import asyncio
+
+
+async def measure_runtime():
+    """measure_runtime should measure the total runtime and return it."""
