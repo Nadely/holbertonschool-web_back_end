b937461a557c6a44109028c9fd3589689616d355
Nadely <6890@holbertonstudents.com>
2024-01-02 15:42:31 +0100

    correction pycode in task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -11,11 +11,11 @@ import typing
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> typing.List[float]:
     """wait_n should return the list of all the delays (float values).
     The list of the delays should be in ascending order without using sort()
     because of concurrency."""
 
-    delay_n = await asyncio.gather(*[wait_random(max_delay)
-                                          for _ in range(n)])
+    delay_n = await asyncio.gather(*[wait_random(max_delay)for _ in range(n)])
     return sorted(delay_n)
