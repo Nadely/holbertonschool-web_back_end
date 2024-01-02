d1b1cc13b2b9d8bc5f78526ceb22ab63461d1b77
Nadely <6890@holbertonstudents.com>
2024-01-02 20:07:56 +0100

    correction in the description on task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-"""Import wait_random from the previous python file that you have written and
-write an async routine called wait_n that takes in 2 int arguments
+"""write an async routine called wait_n that takes in 2 int arguments
 (in this order): n and max_delay. You will spawn wait_random n times with the
 specified max_delay."""
 
