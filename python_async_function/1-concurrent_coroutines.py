059468e8616e307a048e1d180cf4b321a41bddf6
Nadely <6890@holbertonstudents.com>
2024-01-03 22:26:56 +0100

    more description in task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-"""write an async routine called wait_n that takes in 2 int arguments
-(in this order): n and max_delay. You will spawn wait_random n times with the
-specified max_delay."""
-
+"""
+Module to provide an async routine called wait_n that takes in 2 int arguments
+(in this order): n and max_delay. This module spawns wait_random n times with
+the specified max_delay.
+"""
 
 import asyncio
 import typing
@@ -12,9 +13,22 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> typing.List[float]:
-    """wait_n should return the list of all the delays (float values).
-    The list of the delays should be in ascending order without using sort()
-    because of concurrency."""
+    """
+    Perform asynchronous waiting using wait_random.
+
+    Args:
+        n (int): Number of times to call wait_random.
+        max_delay (int): Maximum delay value for each wait_random call.
+
+    Returns:
+        typing.List[float]: List of delays in ascending order.
+
+    Note:
+        This function uses asyncio.gather to concurrently run wait_random n
+        times.
+        The list of delays is returned in ascending order without using sort()
+        due to concurrency.
+    """
 
     delay_n = await asyncio.gather(*[wait_random(max_delay) for _ in range(n)])
 
