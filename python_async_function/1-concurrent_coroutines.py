3e4f07641ef92c1c3c64f3d1673c28fd35bf9b59
Nadely <6890@holbertonstudents.com>
2024-01-03 11:10:12 +0100

    remove task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -3,21 +3,19 @@
 (in this order): n and max_delay. You will spawn wait_random n times with the
 specified max_delay."""
 
+
 import asyncio
-from asyncio import create_task, as_completed
-from typing import List
+import typing
+
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+
+async def wait_n(n: int, max_delay: int) -> typing.List[float]:
     """wait_n should return the list of all the delays (float values).
     The list of the delays should be in ascending order without using sort()
     because of concurrency."""
 
-    tasks = [create_task(wait_random(max_delay)) for _ in range(n)]
+    delay_n = await asyncio.gather(*[wait_random(max_delay) for _ in range(n)])
 
-    delays_n = []
-    for task in as_completed(tasks):
-        delays_n.append(await task)
-
-    return delays_n
+    return sorted(delay_n)
