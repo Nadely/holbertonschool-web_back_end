7a3e2b6e8ffa3c9694589246bbd9737564fe395a
Nadely <nadege.tette@holbertonstudents.com>
2024-01-09 15:51:00 +0100

    correction task 2

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -8,6 +8,9 @@ import math
 from typing import List, Dict
 
 
+index_range = __import__('0-simple_helper_function').index_range
+
+
 class Server:
     """
     Server class to paginate a database of popular baby names.
@@ -29,19 +32,6 @@ class Server:
 
         return self.__dataset
 
-    @staticmethod
-    def index_range(page: int= 1, page_size: int= 10) -> tuple:
-        """
-        Task 0
-        """
-
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
-
-        start = (page - 1) * page_size
-        end = start + page_size
-        return (start, end)
-
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
         return the appropriate page of the dataset
@@ -50,7 +40,7 @@ class Server:
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
-        start, end = self.index_range(page, page_size)
+        start, end = index_range(page, page_size)
 
         return self.dataset()[start:end]
 
