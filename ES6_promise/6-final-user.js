464cfad1573781596e2b1a1a7f715695a5d704e7
Nadely <nadege.tette@holbertonstudents.com>
2024-01-26 02:32:14 +0100

    update task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,9 +1,21 @@
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([
-    signUpUser(firstName, lastName).then(value => ({ status: 'fulfilled', value })),
-    uploadPhoto(fileName).then(value => ({ status: 'fulfilled', value }))
-  ]).catch(error => ({ status: 'rejected', error }));
+const handleProfileSignup = async (firstName, lastName, fileName) => {
+  try {
+    const userPromise = signUpUser(firstName, lastName);
+    const photoPromise = uploadPhoto(fileName);
+
+    const userResult = await userPromise;
+    const photoResult = await photoPromise;
+
+    return {
+      user: { status: 'fulfilled', value: userResult },
+      photo: { status: 'fulfilled', value: photoResult }
+    };
+  } catch (error) {
+    return { status: 'rejected', error };
+  }
 }
+
+export default handleProfileSignup;
