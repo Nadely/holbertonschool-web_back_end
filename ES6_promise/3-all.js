74ce6ced26eb1a2dca5e0340ca71e9e87dd17b42
Nadely <nadege.tette@holbertonstudents.com>
2024-01-26 02:18:01 +0100

    updating task 3 by simplifying it

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,10 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const photoPromise = uploadPhoto();
-  const userPromise = createUser();
-
-  return Promise.all([photoPromise, userPromise])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((value) => {
       console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
     })
