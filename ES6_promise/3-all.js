452906db18a01b463674b018d8cf3422b726edb6
Nadely <nadege.tette@holbertonstudents.com>
2024-01-26 02:11:29 +0100

    updating task 3 by simplifying it

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,14 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function handleProfileSignup() {
-  const photoPromise = uploadPhoto();
-  const userPromise = createUser();
+  try {
+    const [photoResponse, userResponse] = await Promise.all([uploadPhoto(), createUser()]);
 
-  await Promise.all([photoPromise, userPromise])
-    .then((value) => {
-      console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
-    })
-    .catch(() => {
-      console.error('Signup system offline');
-    });
+    console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+  } catch (error) {
+    console.error('Signup system offline');
+  }
 }
