5c58b32b3a6aefc8fccc6ad4e502959be2b5b92e
Nadely <nadege.tette@holbertonstudents.com>
2024-01-26 02:25:12 +0100

    update task 6 add catch

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,6 +1,9 @@
-import signUpUser from './4-user-promise';
-import uplodPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uplodPhoto(fileName)]);
+  return Promise.all([
+    signUpUser(firstName, lastName).then(value => ({ status: 'fulfilled', value })),
+    uploadPhoto(fileName).then(value => ({ status: 'fulfilled', value }))
+  ]).catch(error => ({ status: 'rejected', error }));
 }
