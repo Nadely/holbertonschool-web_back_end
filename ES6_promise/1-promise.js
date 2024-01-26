30bef7147dcfca7393defda1acd0b9a5aa87ecb3
Nadely <nadege.tette@holbertonstudents.com>
2024-01-26 01:53:53 +0100

    update task 1

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,7 +1,8 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
       if (success) {
-        resolve({ status: 200, body: 'Success' });
+        const result = { status: 200, body: 'Success' };
+        resolve(result);
       } else {
         reject(new Error('The fake API is not working currently'));
       }
