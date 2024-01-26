4cdc0a004b8e8c964b492887b2ee2c8efa2612d5
Nadely <nadege.tette@holbertonstudents.com>
2024-01-26 02:06:57 +0100

    update task 2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,13 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve) => {
-    promise
-      .then(() => {
-        const response = { status: 200, body: 'success' };
-        resolve(response);
-      })
-      .catch(() => new Error())
-      .finally(() => {
-        console.log('Got a response from the API');
-      });
-  });
+  return promise
+    .then(() => {
+      const response = { status: 200, body: 'success' };
+      resolve(response);
+    })
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
