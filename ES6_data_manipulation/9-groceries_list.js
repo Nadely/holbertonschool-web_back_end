98ab51574cb5f9821492ebbf2f9664b01029cb63
Nadely <nadege.tette@holbertonstudents.com>
2024-01-29 21:50:55 +0100

    update task 9

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,13 +1,13 @@
-export default function groceriesList(name, quantity) {
+export default function groceriesList() {
   const list = [
     { name: 'Apples', quantity: 10 },
     { name: 'Tomatoes', quantity: 10 },
     { name: 'Pasta', quantity: 1 },
     { name: 'Rice', quantity: 1 },
-    { name: 'Banana', quantity: 5 }
+    { name: 'Banana', quantity: 5 },
   ];
 
-  return new Map(list.map(listValue => [
-    listValue.name, listValue.quantity
+  return new Map(list.map((listValue) => [
+    listValue.name, listValue.quantity,
   ]));
 }
