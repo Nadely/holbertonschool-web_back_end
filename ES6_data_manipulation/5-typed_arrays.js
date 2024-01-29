3feb1a944ad4bf18dfadfc9f7142b2b14496a741
Nadely <nadege.tette@holbertonstudents.com>
2024-01-29 20:34:16 +0100

    update task 5 and simplifying task 7

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,12 +1,12 @@
 export default function createInt8TypedArray(length, position, value) {
-  try {
+  if (position < 0 || position > length) {
+    throw new Error('Position outside range');
+  } else {
     const arrayBuffer = new ArrayBuffer(length);
     const arrayBufferWithInt8 = new Int8Array(arrayBuffer);
 
     arrayBufferWithInt8[position] = value;
 
     return new DataView(arrayBuffer);
-  } catch (error) {
-    throw new Error('Position outside range');
   }
 }
