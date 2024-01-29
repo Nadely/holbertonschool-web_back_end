71dad169e5de17cfc8be0417f65780ffbc709e73
Nadely <nadege.tette@holbertonstudents.com>
2024-01-29 21:16:45 +0100

    add one condition

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,15 @@
 export default function cleanSet(set, startString) {
+  if (startString === '') {
+    return '';
+  }
+
   const valueClean = [];
+
   set.forEach((value) => {
     if (value.startsWith(startString)) {
       valueClean.push(value.slice(startString.length));
     }
   });
+
   return valueClean.join('-');
 }
