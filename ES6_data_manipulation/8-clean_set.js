008c018f85ecddf7186e44a6c05e97b5edd4a6df
Nadely <nadege.tette@holbertonstudents.com>
2024-01-29 22:21:20 +0100

    new method for task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -3,7 +3,24 @@ export default function cleanSet(set, startString) {
     return '';
   }
 
+  return [...set].reduce((result, value) => {
+    let newResult = result; // Create a new variable to hold the updated result
+    if (value.startsWith(startString)) {
+      if (newResult !== '') {
+        newResult += '-';
+      }
+      newResult += value.slice(startString.length);
+    }
+    return newResult; // Return the updated result
+  }, '');
+}
+
+/* export default function cleanSet(set, startString) {
+  if (startString === '') {
+    return '';
+  }
+
   return [...set].filter((value) => value.startsWith(startString))
     .map((value) => value.slice(startString.length))
     .join('-');
-}
+} */
